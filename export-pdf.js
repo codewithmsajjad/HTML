cc2f86ed281d62f1482ea5421a6bfd46d509068d
Muhammad Sajjad <m.sajjad.lwt@gmail.com>
2025-06-19 03:37:30 -0700

    add css

--- export-pdf.js
+++
@@ -1,8 +0,0 @@
-const puppeteer = require('puppeteer');
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('file:///e:/html/resume.html', {waitUntil: 'networkidle0'});
-  await page.pdf({path: 'e:/html/resume.pdf', format: 'A4', printBackground: true});
-  await browser.close();
-})();
